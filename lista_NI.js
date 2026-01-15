7e599d64f34dcdd29a22c9aa0691f015bb20309e
Cleider-F <cleider1000@hotmail.com>
2026-01-15 08:17:46 -0300

    Atualizar o lista_NI.js

--- lista_NI.js
+++ lista_NI.js
@@ -146,7 +146,6 @@ window.LISTA_NI = [
 { produto: "PINO DE CENTRO PEQUENO" , ni: 27019813 },
 { produto: "PORCA DO PINO DE CENTRO" , ni: 27019742 },
 { produto: "PINO DE CENTRO GRANDE" , ni: 27019815 },
-{ produto: "MANGOTE" , ni: 27017950},
 { produto: "MANGUEIRA" , ni: 27017950 }
 
 ];
